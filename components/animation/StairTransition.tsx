08b9f10b4e7e620b8638299aa76998bd81872984
Sinisa <sinisha.stojanovic@gmail.com>
2025-10-18 10:20:05 +0200

    Stairs animatiion refactored to the it's own component

--- components/animation/StairTransition.tsx
+++ components/animation/StairTransition.tsx
@@ -2,17 +2,7 @@
 
 import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
-
-const stairAnimation = {
-  initial: { top: "0%" },
-  animate: { top: "100%" },
-  exit: { top: "0%" },
-};
-
-const reverseIndex = (index: number) => {
-  const totalSteps = 6;
-  return totalSteps - index - 1;
-};
+import Stairs from "@/components/animation/Stairs";
 
 const StairTransition = () => {
   const pathname = usePathname();
@@ -25,21 +15,7 @@ const StairTransition = () => {
           }
         >
           {/* Stairs animation */}
-          {[...Array(6)].map((_, index) => (
-            <motion.div
-              key={index}
-              variants={stairAnimation}
-              initial={"initial"}
-              animate={"animate"}
-              exit={"exit"}
-              transition={{
-                duration: 0.2,
-                ease: "easeInOut",
-                delay: reverseIndex(index) * 0.1,
-              }}
-              className={"h-full w-full bg-white relative"}
-            />
-          ))}
+          <Stairs />
         </div>
 
         <motion.div
