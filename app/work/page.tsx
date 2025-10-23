d8043125dd47b85fcd20d62866b6c58a3c32068a
Sinisa <sinisha.stojanovic@gmail.com>
2025-10-23 10:48:17 +0200

    Work page, added description paragraph to the page, more gap between cards

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -8,9 +8,13 @@ import { motion } from "framer-motion";
 const WorkPage = () => {
   return (
     <Section>
-      <div className={"container mx-auto flex-1"}>
+      <div className={"container mx-auto flex-1 "}>
         {/*<div className={"px-4 py-8"}>*/}
-        <h1 className={"text-2xl font-semibold mb-6"}>Work</h1>
+        <h1 className={"text-2xl text-accent font-semibold mb-6"}>Work</h1>
+
+        {/* todo: Add some intro text for work section */}
+        <p className={"mb-12"}>This are my project I have done so far</p>
+
         <motion.div
           initial={{ opacity: 0 }}
           animate={{
@@ -18,7 +22,7 @@ const WorkPage = () => {
             transition: { delay: 2, duration: 0.4, ease: "easeIn" },
           }}
           // className={"w-full flex gap-10"}
-          className="grid gap-6 grid-cols-1 justify-center sm:grid-cols-2 lg:grid-cols-3"
+          className="grid gap-10 grid-cols-1 justify-center sm:grid-cols-2 lg:grid-cols-2"
         >
           {projects.map((project) => (
             <ProjectCard key={project.slug} project={project} />
